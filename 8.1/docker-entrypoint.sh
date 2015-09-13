84613f1ac166e3b87d16d79627d5c6bd245a2dc2
Remi Hakim <remi@datadoghq.com>
2015-09-13 00:50:38 -0400

    Update docker-entrypoint.sh

--- 8.1/docker-entrypoint.sh
+++ 8.1/docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
-
-if [ ! -e '/var/www/html/version.php' ]; then
+mkdir -p /var/www/html/owncloud
+cd /var/www/html/owncloud
+if [ ! -e '/var/www/html/owncloud/version.php' ]; then
 	tar cf - --one-file-system -C /usr/src/owncloud . | tar xf -
 	chown -R www-data /var/www/html
 fi
